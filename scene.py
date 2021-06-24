fc88333969b47d28962d8b5c4804753efdcae26f
PrestonFawcett <ptfawcett@csu.fullerton.edu>
2021-06-24 10:09:32 -0700

    Update class Scene

--- scene.py
+++ scene.py
@@ -1,18 +1,23 @@
 import pygame
 
 class Scene():
-    def __init__(self, scene_id):
+    def __init__(self, scene_id, screen, background_color):
         self._id = scene_id
+        self._screen = screen
+        self._background = pygame.Surface(self._screen.get_size())
+        self._background.fill(background_color)
         self._is_valid = True
         self._frame_rate = 60
 
     def draw(self):
-        pass
+        if self._screen:
+            self._screen.blit(self._background, (0, 0))
 
     def process_event(self, event):
         if event.type == pygame.QUIT:
+            print('Quiting program')
             self.set_not_valid()
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             self.set_not_valid()
 
     def is_valid(self):
