04877c21a99d854a305f2bf9cd4263da6239d42d
PrestonFawcett <ptfawcett@csu.fullerton.edu>
2021-06-24 10:23:10 -0700

    Added base colors

---
+++ colors.py
@@ -0,0 +1,13 @@
+""" List of colors """
+
+black = (0, 0, 0)
+red = (255, 0, 0)
+orange = (255, 128, 0)
+yellow = (255, 255, 0)
+darkgreen = (0, 153, 0)
+green = (0, 255, 0)
+teal = (0, 255, 255)
+blue = (0, 0, 255)
+purple = (127, 0, 255)
+magenta = (255, 0, 255)
+white = (255, 255, 255)
